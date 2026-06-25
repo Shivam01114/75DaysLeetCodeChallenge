3f189c1ec582cbda0f4f8406a9a9012c054439ad
Shivam Singh <141226994+Shivam01114@users.noreply.github.com>
2026-06-25 12:34:46 +0530

    Time: 272 ms (12.51%), Space: 19.3 MB (43.14%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<int> arr(n + 1, 0);
-        for (int i = 0; i <= n; i++) {
-            int left = max(0, i - ranges[i]);
-            int right = min(n, i + ranges[i]);
-            arr[left] = max(arr[left], right);
+        vector<int> arr(n+1,0);
+        for (int i = 0;i<=n;i++) {
+            int l = max(0,i-ranges[i]);
+            int r = min(n,i+ranges[i]);
+            arr[l] = max(arr[l],r);
         }
-
-        int count = 0;
-        int curr = 0;
-        int maxi = 0;
-        for (int i = 0; i < n; i++) {
-            if (i > maxi) return -1; 
-
-            maxi = max(maxi, arr[i]);
-
-            if (i == curr) {
+        for (int x : arr)  cout  << x << " ";
+        cout << endl;
+        for (int i = 0;i<=n;i++) cout << i << " ";
+        int maxi = 0, curr = 0, count = 0;
+        for (int i  =0;i<arr.size()-1;i++) {
+            maxi = max(maxi,arr[i]);
+            if (i ==  curr) {
                 count++;
                 curr = maxi;
             }
         }
-
-        return curr >= n ? count : -1;
+        if (curr<n) return -1;
+        return count;
     }
 };
