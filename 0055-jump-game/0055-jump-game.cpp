4c579982bfbf5bdc8979017aafb3fc4d82341c42
Shivam Singh <141226994+Shivam01114@users.noreply.github.com>
2026-06-25 10:39:13 +0530

    Time: 2 ms (29.29%), Space: 52.4 MB (26.58%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -6,10 +6,9 @@ public:
         
         for (int i = 0; i < n; i++) {
             if (i > maxReach) return false;
-            if (maxReach >= n - 1) return true;
             maxReach = max(maxReach, i + nums[i]);
         }
         
-        return false;
+        return true;
     }
 };
