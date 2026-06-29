4be63ac7f2150002fec5b062f34f109268f62357
Shivam Singh <141226994+Shivam01114@users.noreply.github.com>
2026-06-29 10:07:06 +0530

    Time: 0 ms (100%), Space: 7.9 MB (55.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,46 @@
 class Solution {
-    vector<int> dp;
+    // vector<int> dp;
+// public:
+//     int climbStairs(int n) {
+//         dp.resize(n+1,-1);
+//         return find(n);
+//     }
+//     int find(int n) {
+//         if (n == 0) return 1;
+//         if (n  < 0) return 0;
+//         if (dp[n] != -1) return dp[n];
+//         return dp[n] = find(n-1) + find(n-2);
+//     }
+// };
+
+
+//Using For Loop
+// public:
+//     int climbStairs(int n){
+//         vector<int>dp(n+1,0);
+//         dp[0] = 1;
+//         dp[1] = 1;
+//         for(int i = 2;i<=n;i++){
+//             dp[i] = dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// };
+
+// Using two Pointer
 public:
     int climbStairs(int n) {
-        dp.resize(n+1,-1);
-        return find(n);
-    }
-    int find(int n) {
-        if (n == 0) return 1;
-        if (n  < 0) return 0;
-        if (dp[n] != -1) return dp[n];
-        return dp[n] = find(n-1) + find(n-2);
+        if (n <= 2) return n;
+        
+        int first = 1;  
+        int second = 2; 
+        
+        for (int i = 3; i <= n; i++) {
+            int current = first + second;
+            first = second;
+            second = current;
+        }
+        
+        return second;
     }
 };
